80159b38d64c246b5e584b3ffe17f948fd2d7f8a
gssdromen <gssdromen@126.com>
2014-11-02 03:25:51 +0800

    a

--- Binary Tree Level Order Traversal/main.py
+++ Binary Tree Level Order Traversal/main.py
@@ -5,8 +5,31 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
+    def __init__(self):
+        self.result = []
+        self.curList = []
+        self.nextList = []
+        
     # @param root, a tree node
     # @return a list of lists of integers
     def levelOrder(self, root):
+        if root is None:
+            return self.result
+        self.curList.append(root)
+        self.helper(self.curList)
+        return self.result
+
+    def helper(self, nodeList):
+        del self.nextList[:]
+        self.nextList[:] = []
+        answer = []
+        for node in self.curList:
+            if node is not None:
+                answer.append(node.val)
+                self.nextList.append(node.left)
+                self.nextList.append(node.right)
+        self.result.append(answer)
+        self.helper(self.nextList)
 
