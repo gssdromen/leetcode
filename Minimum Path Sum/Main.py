1bd2b5e8c0620b1b7312e0af898cc337facac02d
Wu Cedric <gssdromen@126.com>
2014-11-23 07:47:27 +0800

    aa

---
+++ Minimum Path Sum/Main.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @param grid, a list of lists of integers
+    # @return an integer
+    def minPathSum(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+
+
+ss = Solution()
+ss.minPathSum([[1,2,3,4]])
+print "aa"
+print "ss"
