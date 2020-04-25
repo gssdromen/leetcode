02a07e53be1a98204efc4f4bba3c0199bab86d15
gssdromen <example@xx.com>
2020-04-25 22:05:46 +0800

    new one

---
+++ 226. 翻转二叉树/main.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root == None:
+            return root
+
+        if root.left != None:
+            self.invertTree(root.left)
+        if root.right != None:
+            self.invertTree(root.right)
+        root.left, root.right = root.right, root.left
+    
+        return root
