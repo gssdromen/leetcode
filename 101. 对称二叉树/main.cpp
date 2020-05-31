6ebeb5588c7bdc976b4ba92557f2e5086ef3f0e3
gssdromen <example@xx.com>
2020-05-31 14:28:10 +0800

    新题目

---
+++ 101. 对称二叉树/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr)
+        {
+            return true;
+        }
+        
+        return helper(root->left, root->right);
+    }
+
+    bool helper(TreeNode* left, TreeNode* right) {
+        if (!left && !right) 
+        {
+            return true;
+        }
+        if (!left || !right)
+        {
+            return false;
+        }
+
+        bool flag1 = left->val == right->val;
+        bool flag2 = helper(left->left, right->right);
+        bool flag3 = helper(left->right, right->left);
+
+        return flag1 && flag2 && flag3;
+    }
+};
+
+int main() {
+    Solution s = Solution();
+    
+    TreeNode root = TreeNode(3);
+    TreeNode left = TreeNode(1);
+    TreeNode right = TreeNode(1);
+    root.left = &left;
+    root.right = &right;
+
+    cout << "a" << s.isSymmetric(&root) << "b" << endl;
+    cout << "fasdf";
+    return 0;
+}
