005915dec9d99aed9502452d365ac8c49195e395
wuyinjun.oreki <wuyinjun.oreki@bytedance.com>
2021-10-18 08:53:14 +0800

    update

--- 快速排序/main.rs
+++ 快速排序/main.rs
@@ -1,9 +1,45 @@
 impl Solution {
-    pub fn sort_array(nums: Vec<i32>) -> Vec<i32> {
-
+    pub fn sort_array(num: Vec<i32>) -> Vec<i32> {
+        return vec![];
     }
 
-    fn quick_sort(nums: Vec<i32>) -> Vec<i32> {
-        
+    fn quick_sort(num: &Vec<i32>) -> &Vec<i32> {
+        if num.len() < 1 {
+            return &vec![];
+        }
+        if num.len() == 1 {
+            return num;
+        }
+        let mid = Solution::partition(&num, 0, num.len() - 1);
+        let left_slice = Solution::quick_sort(num[..mid]);
+        let right_slice = Solution::quick_sort(num[mid+1..]);
+        let temp = [left_slice, vec![num[mid]], right_slice].concat();
+        return temp;
     }
+
+    fn partition(num: &Vec<i32>, left: usize, right: usize) -> usize {
+        let pivot = num[right];
+        let i = left;
+        for j in left .. right {
+            if num[j] > pivot {
+                Solution::swap_here(num, i, j);
+                i += 1;
+            }
+        }
+        Solution::swap_here(num, i, right);
+        return i;
+    }
+
+    fn swap_here(num: &Vec<i32>, x: usize, y: usize) {
+        let temp = num[x];
+        num[x] = num[y];
+        num[y] = temp;
+    }
+}
+struct Solution {
+
+}
+
+fn main() {
+
 }
